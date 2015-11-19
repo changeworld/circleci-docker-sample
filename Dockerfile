61065fb69569a6dcd633e9e08712b8be0f8eea8d
changeworld <changesworlds@gmail.com>
2015-11-19 18:44:32 +0900

    Modify Dockerfile
    Install packages for JDK, Scala, npm, Node.js
    ref #1

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,14 @@
 FROM ubuntu
 MAINTAINER Takashi Takebayashi <changesworlds@gmail.com>
 
-RUN apt-get update && \
- apt-get install -y nginx
+# Install packages for JDK, Scala, npm, Node.js
+RUN \
+ apt-get update && \
+ apt-get install -y --force-yes \
+  curl \
+  default-jdk \
+  git \
+  nodejs \
+  npm \
+  scala \
+  wget
--- spec/server/apt-get_spec.rb
+++ spec/server/apt-get_spec.rb
@@ -8,7 +8,31 @@ if ENV['CIRCLECI']
 end
 
 describe 'apt-get' do
-  describe package('nginx') do
+  describe package('curl') do
+    it { should be_installed }
+  end
+
+  describe package('default-jdk') do
+    it { should be_installed }
+  end
+
+  describe package('git') do
+    it { should be_installed }
+  end
+
+  describe package('nodejs') do
+    it { should be_installed }
+  end
+
+  describe package('npm') do
+    it { should be_installed }
+  end
+
+  describe package('scala') do
+    it { should be_installed }
+  end
+
+  describe package('wget') do
     it { should be_installed }
   end
 end
